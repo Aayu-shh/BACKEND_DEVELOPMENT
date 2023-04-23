2f68675110fad27cd023a3c3d2c305667217349f
Aayu-shh <aayush28.official@gmail.com>
2023-04-23 19:52:15 +0530

    Task3 - Map method to Transform Fruits

--- ClassThis_fatArrow.js
+++ ClassThis_fatArrow.js
@@ -12,31 +12,45 @@
 
 // student.greet();
 
-class Student{
-    constructor(name,age,marks){
-        this.name = name;
-        this.age = age;
-        this.marks = marks;
-    }
+// class Student{
+//     constructor(name,age,marks){
+//         this.name = name;
+//         this.age = age;
+//         this.marks = marks;
+//     }
 
-    setPlacementAge(minPlacementAge){
-        //console.log(this)
-        return  (minMarks)=>{                                   //Normal function if returned -- gives 'this' as undefined inside it
-            if(this.marks>minMarks &&this.age>minPlacementAge){
-                console.log(this.name,"is ready for placement");
-            }
+//     setPlacementAge(minPlacementAge){
+//         //console.log(this)
+//         return  (minMarks)=>{                                   //Normal function if returned -- gives 'this' as undefined inside it
+//             if(this.marks>minMarks &&this.age>minPlacementAge){
+//                 console.log(this.name,"is ready for placement");
+//             }
         
-        else{
-            console.log(this.name, "is NOT ready for placement");
-            }  
-        };
-    }
-}
+//         else{
+//             console.log(this.name, "is NOT ready for placement");
+//             }  
+//         };
+//     }
+// }
 
 
 
-//Instantiating 
-const Yash = new Student('Yash',25,75);
-const Vaibhav = new Student('Vaibhav',13,35);
+// //Instantiating 
+// const Yash = new Student('Yash',25,75);
+// const Vaibhav = new Student('Vaibhav',13,35);
 
-Yash.setPlacementAge(18)(40);              //placementAge and Eligible minMarks - set by distinct companies
+// Yash.setPlacementAge(18)(40);              //placementAge and Eligible minMarks - set by distinct companies
+
+let fruits = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
+
+let fruitsTransformed = fruits.map(fruit=>{
+    if(fruit==' ')
+        return 'empty string';
+    else
+        return fruit;
+
+        
+});
+console.log(fruits);
+console.log(fruitsTransformed);
+
