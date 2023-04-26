5273f6cbcba904ea9f1aa5df0428a09e786d3b71
Aayu-shh <aayush28.official@gmail.com>
2023-04-26 09:28:23 +0530

    Task7_Rendering_HTML_page_using res.write()

--- app.js
+++ app.js
@@ -13,8 +13,16 @@ const myName = "Aayush";
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log(myName);
-    console.log(res);
+    console.log("URL: ",req.url," \n Method:",req.method,"\n HEADERSSS: ",req.headers);
+    
+    res.setHeader('Content-Type','text/html');
+    //.write() works in chunks like -
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write("<body style='text-align: center'><h1> Hello from my Node.js Server!!</h1></body>");    
+    res.write('</html>');
+    res.end();      //No .write() method now --> if written ERROR thown
+    //process.exit();
 })
 
 server.listen(4000);
