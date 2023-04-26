60791f3018450f33dfb9b77ea1d29b4354d10976
Aayu-shh <aayush28.official@gmail.com>
2023-04-26 09:42:00 +0530

    Task7_Coninued_url=/

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const myName = "Aayush";
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log("URL: ",req.url," \n Method:",req.method,"\n HEADERSSS: ",req.headers);
-    
+    const url = req.url;
+    if(url == '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write("<body><form action='/message' method ='POST'><input type='text' name='msg'><button type='submit'>Send</button></form></body>");
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html');
     //.write() works in chunks like -
     res.write('<html>');
