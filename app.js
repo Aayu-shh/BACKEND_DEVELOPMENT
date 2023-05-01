f9b4628b9f6290fc7857a6a6a0ef7feae6677b29
Aayu-shh <aayush28.official@gmail.com>
2023-05-01 20:06:07 +0530

    Task2_MiddleWares2 BodyParser

--- app.js
+++ app.js
@@ -11,16 +11,25 @@ os
 //require() - import files/paths/modules
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the Middleware');
-    next();
+//MiddleWare function
+//Automatically calls next()
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><label>Poduct Name:</label><input type="text" name="title"><br><label>Poduct Size:</label><input type="text" name = "size"><br><button type="submit">ADD PRODUCT</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In Another Middleware');
+//Can be placed anywhere just before last "/" Only enters incase of POST requests
+app.post('/product',(req,res,next)=>{                                    
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
