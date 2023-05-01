b1a9ae639d25dcbd416c22e056b4a7fd10a11ec3
Aayu-shh <aayush28.official@gmail.com>
2023-05-01 17:55:19 +0530

    Task1_ExpressJs

--- app.js
+++ app.js
@@ -10,9 +10,18 @@ os
 
 //require() - import files/paths/modules
 
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');         //Holds requestHandler function
+const app = express();
 
-const server = http.createServer(routes.handler);
-server.listen(4000);
+app.use((req,res,next)=>{
+    console.log('In the Middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In Another Middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(4000);
