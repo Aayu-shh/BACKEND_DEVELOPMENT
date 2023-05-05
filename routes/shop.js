2701efd96c19bf4d07b2e2bb518d28be99beb7c1
Aayu-shh <aayush28.official@gmail.com>
2023-05-05 09:44:47 +0530

    Task5_Added PathJS

--- routes/shop.js
+++ routes/shop.js
@@ -6,13 +6,13 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
-
-
 
 // /shop/ => GET
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 })
 
 module.exports = router;
